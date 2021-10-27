362281810d5e4141da36bb6aac12618f88f4ed56
Mr. Dynamic <93033492+Mr-Dynamic@users.noreply.github.com>
2021-10-27 19:47:46 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,68 @@
+class Translation(object):
+
+      
+      START_TEXT = """
+😃 Hai {},
+I am a simple auto caption bot by @DynamicHub
+I can automatically add pre-setted caption to the files in channels
+I am currently working for a channel if you want to use me, Use my source code and Deploy me
+⚜️ Maintained By 👉 @{}
+"""    
+
+      HELP_TEXT = """
+<u>💡 𝐇𝐞𝐥𝐩</u> 
+👉 Add me as a Admin in your channel with edit permission
+👉 Add your caption using <code>/set_caption</code> command
+<i>( Only Admin )</i>
+   
+<i>(Support Markdown)</i>
+👉 Forward your files in your channel and I will edit it
+<b><a href="https://t.me/DynamicHub">©️ Dynamic Hub</a></b>
+"""    
+              
+
+     
+      ABOUT_TEXT = """
+📕 --𝐀𝐛𝐨𝐮𝐭-- --𝐌𝐞--
+○ ᴍʏ ɴᴀᴍᴇ : [Auto Caption](https://t.me/DynamicHub)
+○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
+○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
+○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
+○ ᴠᴇʀsɪᴏɴ : 1.1.0
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : [ᴛᴏᴜᴄʜ ᴍᴇ 🤗](https://github.com/Mr-Dynamic/AutoCaption)
+○ ᴄʀᴇᴀᴛᴏʀ :  [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio)
+ 
+__ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀɴᴅ ᴛʜɪs ɪs ᴍʏ ғɪʀsᴛ ᴛɪᴍᴇ ɪ ᴀᴍ ɢɪᴠɪɴɢ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ ʙᴏᴛ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ ᴡʜɪᴄʜ ᴡᴀs ᴡʀɪᴛᴇʀ ɪɴ ᴍʏ ᴏᴡɴ ʜᴀɴᴅs, sᴏ ɪғ ᴀɴʏ ʙᴜɢs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ sᴀʏ
+ɪᴛ ᴍᴀʏ ʙᴇ ɴᴏᴛ ᴛʜᴇ ʙᴇsᴛ ʙᴜᴛ ғᴏʀ ᴍᴇ ɪᴛ ɪs ᴛʜᴇ ʙᴇsᴛ 🥰
+[©️ DynamicHub](https://t.me/dynamichub)**
+"""
+
+      MARKDOWN_TEXT = """
+🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
+👉 <b>Bold text</b>
+      
+📌 <code>**text**</code> 
+👉 <b>Italic text</b>
+📌 <code>__text__</code> 
+👉 <b>Code text</b>
+📌 <code>`text`</code>   
+👉 <b>Hyperlink text</b>
+📌 <code>[hyperlink_text](https://t.me/dynamichub)</code> 
+<b><a href="https://t.me/dynamichub">©️ Dynamic Hub</a></b>
+"""
+
+# Bot status display
+
+      STATUS_DATA = """
+🔰 <u>𝐁𝐎𝐓 𝐒𝐓𝐀𝐓𝐔𝐒</u>
+🖌️ <b>Current Caption :</b>
+{}
+📐 <b>Current Position :</b> {}
+<b><a href="https://t.me/Dynamichub">©️ Dynamic Hub</a></b>
+"""
+
+      NOT_ADMIN_TEXT = """
+🔰 <u>𝐒𝐎𝐑𝐑𝐘</u>
+Only my admin can see my status, if you want this bot you can take my source code and deploy it. 
+<b><a href="https://t.me/DynamicHub">©️ Dynamic Hub</a></b>
+""
